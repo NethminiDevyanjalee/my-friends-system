70dd5d56444ae11701230bd1809bd20afc3d4d85
NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
2022-03-09 19:08:24 +0530

    implemented home page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html  lang="en">
+    <head>
+        <meta charset='UTF-8'>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h2>My Friend System <br> Assignment Home Page</h2>
+        <p>Name: D.D.W Nethmini Devyanjalee&nbsp;&nbsp;&nbsp;&nbsp;Student ID: SE/2018/012<br>
+            Email: <a href="nethminid1999@gmail.com">nethminid1999@gmail.com</a></p>
+        <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source</p>
+        <p>Table successfully created and populated.</p>
+        <a href="sign-up.php">Sign-Up</a>&nbsp;&nbsp;<a href="log-in.php">Log-In</a>&nbsp;&nbsp;<a href="about.php">About</a>
+    </body>
+</html>
